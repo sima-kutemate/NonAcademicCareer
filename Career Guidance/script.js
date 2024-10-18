4c5ecb0685b2b6415b244b18e0a3cb29882d87fd
sima-kutemate <simakutemate@gmail.com>
2024-10-18 10:43:39 +0530

    update home file

--- Career Guidance/script.js
+++ Career Guidance/script.js
@@ -4,4 +4,19 @@ let navbar = document.querySelector('.header .navbar');
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
+};
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
