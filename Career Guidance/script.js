91454bd70aa5f4c8908f023f6f1f627dfdfe01e0
sima-kutemate <simakutemate@gmail.com>
2024-10-20 21:24:24 +0530

    some code remove

--- Career Guidance/script.js
+++ Career Guidance/script.js
@@ -22,19 +22,3 @@ var swiper = new Swiper(".home-slider", {
       prevEl: ".swiper-button-prev",
     },
   });
-
-
-// var swiper = new Swiper(".review-slider", {
-//     loop:true,
-//     breakpoints: {
-//       640: {
-//         slidesPerView: 1,
-//       },
-//       768: {
-//         slidesPerView: 2,
-//       },
-//       1024: {
-//         slidesPerView: 3,
-//       },
-//     },
-//   });
